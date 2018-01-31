15ac515ec888c44145f2bd05fea4316ddf3ecd75
Lefebvre Jeremy <lj303991@etu.unice.fr>
2018-01-31 12:12:09 +0100

    Fix des fonctions mathématiques. TODO : Finir le return des Measurements

--- groovy/src/main/groovy/Test.groovy
+++ groovy/src/main/groovy/Test.groovy
@@ -1,9 +1,8 @@
 law "random1" ofType "Random"
 
-
 law "markov1" ofType "MarkovChain" states (["state1", "state2"]) transitions "state1" to "state2" with 0.2 and "state2" to "state1" with 0.7
 
-law "file1" ofType "File" fromPath "/home/afusco/Cours/security/SSL-al/groovy/src/main/resources/rawdata/data1.csv" format "CSV" withColumns ([t: 0, s: 1, v: 8])
+law "file1" ofType "File" fromPath "/home/ringo/Bureau/SSL-al/groovy/src/main/resources/rawdata/data1.csv" format "CSV" withColumns ([t: 0, s: 1, v: 8])
 
 addNoise "file1", -5, 5
 addOffset  "file1", 10
--- kernel/src/main/java/kernel/structural/laws/MathFunctionLaw.java
+++ kernel/src/main/java/kernel/structural/laws/MathFunctionLaw.java
@@ -4,39 +4,49 @@ import kernel.Measurement;
 
 import java.time.Instant;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class MathFunctionLaw implements Law {
 
-    public DomainType domain;
-    //Formules maths
-    public String formuleInegalite;
-
-    ;
-    public String operator;
-    public ArrayList<FunctionExpression> functions;
     private String name;
-    public MathFunctionLaw() {
-        functions = new ArrayList<>();
+
+    //Valeurs retournees
+    public enum DomainType {BOOL,STRING,INT};
+    public DomainType domain;
+
+    //Formules <valeur : condition>
+    public Map<String,String> funcs;
+
+    public MathFunctionLaw(){
+        funcs = new HashMap<>();
     }
 
     public void setDomain(DomainType domain) {
         this.domain = domain;
     }
 
-    public void addFunctionExpression(String s) {
-        String[] tmp = s.split("si");
-        FunctionExpression f = new FunctionExpression(tmp[0], tmp[1]);
-        f.findInegaliteRight();
-        f.findFormuleLeft();
+    public void addFunction(String value, String condition){
+       funcs.put(value,condition);
     }
 
     @Override
     public Measurement generateNextMeasurement(int t) {
-        int value = -1;
-        for (FunctionExpression f : functions) {
-            if (f.calculateRight(t)) {
-                value = f.calculateLeft(t);
-            }
+        Object value = -1;
+        Iterator mapIterator = this.funcs.entrySet().iterator();
+        switch (domain){
+            case INT:
+                //Boucle over map
+                //doEvalConditions
+                //if condition -> doEvalValue
+                //return value and BREAK
+
+            default:
+                //Boucle over map
+                //doEvalConditions
+                //if cond -> return  and BREAK
+
         }
 
         return new Measurement("name", System.currentTimeMillis(), value);
@@ -49,11 +59,7 @@ public class MathFunctionLaw implements Law {
 
     @Override
     public void setName(String name) {
-        this.name = name;
+        this.name=name;
     }
 
-    //Valeurs retournees
-    public enum DomainType {
-        BOOL, STRING, INT
-    }
 }
